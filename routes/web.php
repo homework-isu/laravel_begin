144e7d3c2979a1df4c982a389e9dd4d2cfea3353
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-11-27 18:18:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\RecordController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
